10d8088f77ee7ce38e9f5979446809be2c46537b
Pari <parigupta7415.nvps@gmail.com>
2024-11-27 23:43:08 +0530

    message frontend complete

--- Frontend/src/home/right/Messages.jsx
+++ Frontend/src/home/right/Messages.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import Message from './Message'
 import useGetMessage from "../../context/useGetMessage.js"
 import Loading from '../../../components/Loading.jsx';
@@ -7,18 +7,28 @@ function Messages() {
     const {messages, loading }=useGetMessage();
 
     console.log(messages);
+    const lastMessageRef = useRef()
+    useEffect(()=>{
+        setTimeout(()=>{
+           if(lastMessageRef.current){
+            lastMessageRef.current.scrollIntoView({ behavior : "smooth"})
+           }
+        }, 100)
+    },[messages])
     return (
         <>
 
 {loading?(<Loading></Loading>):(messages.length>0 && messages.map((message)=>{
-    return <Message key={message._id} message={message} />
+    return <Message
+     key={message._id} 
+     message={message} />
 }
 ) )}
          <div className='' style={{minHeight: "calc(88vh - 10vh)"}}>
 
             {!loading && messages.length===0 && 
             <div>
-                <p className='text-center font-bold mt-[20%]'>Say! Hi</p>
+                <p className='text-center font-sans mt-[20%]'>Say! Hi</p>
             </div>    }
          </div>
          
